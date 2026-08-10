3f2320634c41f09b293385762923b8d6566c43d3
Tobias <odilsonluz38@gmail.com>
2026-08-10 11:56:12 -0300

    Delete EXER03 directory

--- EXER03/script.js
+++
@@ -1,7 +0,0 @@
-function area(){
-    var a = parseFloat(document.getElementById("lado1").value)
-    var b = parseFloat(document.getElementById("lado2").value)
-    var resp = document.getElementById("resposta")
-    resp.innerHTML = (a * b).toFixed(2)
-}
-
